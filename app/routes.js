00012971092b664d91e47b866245ca42842ef05c
danleech-defra <danleech.defra@gmail.com>
2017-01-09 11:24:43 +0000

    Scenarios chnages

--- app/routes.js
+++ app/routes.js
@@ -32,28 +32,20 @@ module.exports = {
       res.render('station/5090-flooding-possible', { 'section': section, 'section_name': sectionName, 'page_name': pageName })
     })
 
-    // Example station: Below normal range
-    app.get('/station/5090-below-normal-range', function (req, res) {
-      var section = 'station'
-      var sectionName = 'Station'
-      var pageName = 'Greta at Low Briery'
-      res.render('station/5090-below-normal-range', { 'section': section, 'section_name': sectionName, 'page_name': pageName })
-    })
-
-    // Example station: No forecast data available
-    app.get('/station/5090-no-forecast-data', function (req, res) {
-      var section = 'station'
-      var sectionName = 'Station'
-      var pageName = 'Greta at Low Briery'
-      res.render('station/5090-no-forecast-data', { 'section': section, 'section_name': sectionName, 'page_name': pageName })
-    })
-
-    // Example station: Tidal
+    // Example station: Tidal normal range
     app.get('/station/5205', function (req, res) {
       var section = 'station'
       var sectionName = 'Station'
       var pageName = 'Irish Sea at Silloth Docks'
       res.render('station/5205', { 'section': section, 'section_name': sectionName, 'page_name': pageName })
+    })
+
+    // Example station: Tidal flooding possible
+    app.get('/station/5205-flooding-possible', function (req, res) {
+      var section = 'station'
+      var sectionName = 'Station'
+      var pageName = 'Irish Sea at Silloth Docks'
+      res.render('station/5205-flooding-possible', { 'section': section, 'section_name': sectionName, 'page_name': pageName })
     })
 
   }
